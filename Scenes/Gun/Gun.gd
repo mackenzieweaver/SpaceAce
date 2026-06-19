f91abf3435131bbacb396abfd1027969a2d8157e
Mackenzie Weaver <macknz7@gmail.com>
2026-06-19 12:00:43 -0400

    gun shoot and hit vfx

---
+++ Scenes/Gun/Gun.gd
@@ -0,0 +1,35 @@
+extends Node3D
+class_name Gun
+
+
+@export var debounce: float = 0.18
+@export var sound_effect: AudioStream
+@export var muzzles: Array[Node3D]
+
+
+const GUN_FLASH = preload("res://Scenes/VFX/GunFlash/GunFlash.tscn")
+@onready var effect: AudioStreamPlayer3D = $Effect
+
+
+var _gun_flashes: Array[GPUParticles3D]
+var _timer: float = 0.0
+
+
+func shoot():
+	if _timer > debounce:
+		effect.play()
+		for gf in _gun_flashes:
+			gf.emitting = true
+		_timer = 0.0
+
+
+func _ready() -> void:
+	effect.stream = sound_effect
+	for m in muzzles:
+		var gf: GPUParticles3D = GUN_FLASH.instantiate()
+		m.add_child(gf)
+		_gun_flashes.append(gf)
+
+
+func _process(delta: float) -> void:
+	_timer += delta
---
+++ Scenes/VFX/ImpactFlash/ImpactFlash.gd
@@ -0,0 +1,18 @@
+extends GPUParticles3D
+
+@onready var impact_sound: AudioStreamPlayer3D = $ImpactSound
+
+@export var one_off: bool = true
+
+func bang():
+	impact_sound.play()
+	emitting = true
+	
+	if one_off:
+		await impact_sound.finished
+		queue_free()
+
+func _ready() -> void:
+	if one_off:
+		bang()
+
