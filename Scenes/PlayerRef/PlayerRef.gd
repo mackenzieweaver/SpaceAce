4a09e183b28b4534e20e83ca144ae4d7abe8a194
Mackenzie Weaver <macknz7@gmail.com>
2026-06-19 12:19:49 -0400

    fixing player ref code by updating player position every tick

--- Scenes/PlayerRef/PlayerRef.gd
+++ Scenes/PlayerRef/PlayerRef.gd
@@ -2,16 +2,19 @@ extends Node
 class_name LinkPlayer
 
 var player: Player
+var player_pos: Vector3
+var player_x: float
+var player_y: float
+var player_z: float
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group(Player.GROUP_PLAYER)
 
-var player_pos: Vector3:
-	get: return player.global_position if player else Vector3.ZERO
-
-var player_x: float = player_pos.x
-var player_y: float = player_pos.y
-var player_z: float = player_pos.z
+func _physics_process(_delta: float) -> void:
+	player_pos = player.global_position
+	player_x = player_pos.x
+	player_y = player_pos.y
+	player_z = player_pos.z
 
 func player_pos_set_y(y: float) -> Vector3:
 	return Vector3(player_x, y, player_z)
@@ -21,3 +24,14 @@ func player_too_close(from: Vector3) -> bool:
 	return from.distance_to(player_pos) < 0.05
 func player_less_than_distance(from: Vector3, distance: float) -> bool:
 	return from.distance_to(player_pos) < distance
+
+
+
+
+
+
+
+
+
+
+
